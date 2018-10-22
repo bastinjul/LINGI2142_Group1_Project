a4dc1db785857944d6a7bd6fcbf287145e0a1b9f
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-22 15:54:22 +0000

    config snmp ok

--- scripts/config_snmp.py
+++ scripts/config_snmp.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import json
 import shutil
+import os
 
-from constants import PREFIXES, SCRIPTS_PATH, MAIN_PATH, SNMP_DIR_PATH
+from constants import PREFIXES, SCRIPTS_PATH, MAIN_PATH, SNMP_FILE_PATH
 
 with open(SCRIPTS_PATH+'configuration_router.json') as data_file:
     data = json.load(data_file)
 
 for r in data:
-    shutil.copytree(SNMP_DIR_PATH, MAIN_PATH + 'ucl_minimal_config/'+r)
+	if not os.path.exists(MAIN_PATH + 'ucl_minimal_cfg/'+r+'/snmp/'):
+		os.makedirs(MAIN_PATH + 'ucl_minimal_cfg/'+r+'/snmp/')
+	shutil.copy(SNMP_FILE_PATH,MAIN_PATH + 'ucl_minimal_cfg/'+r+'/snmp/')
--- scripts/constants.py
+++ scripts/constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-MAIN_PATH = "~/LINGI2142_Group1_Project/"
-SCRIPTS_PATH = "~/LINGI2142_Group1_Project/scripts/"
-SNMP_DIR_PATH = "~/LINGI2142_Group1_Project/Monitoring/snmp/"
+MAIN_PATH = "/home/vagrant/LINGI2142_Group1_Project/"
+SCRIPTS_PATH = "/home/vagrant/LINGI2142_Group1_Project/scripts/"
+SNMP_FILE_PATH = "/home/vagrant/LINGI2142_Group1_Project/Monitoring/snmp/snmpd.conf"
 PREFIXES = ["fd00:200:1:", "fd00:300:1:"]
 
