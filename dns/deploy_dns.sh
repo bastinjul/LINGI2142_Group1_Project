9ff26f4b617e24c00a77f8a5de91a3d7dfa089cb
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-27 20:31:06 +0000

    small changes dns

--- dns/deploy_dns.sh
+++ dns/deploy_dns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT='/home/vagrant/lingi2142'
+ROOT='/home/vagrant/LINGI2142_Group1_Project'
 sudo mkdir -p $ROOT/dns/zones
 #sudo bash $ROOT/dns_config_creation.sh
 for i in 1 2
@@ -8,9 +8,9 @@ do
  sudo mkdir -p $ROOT/ucl_minimal_cfg/NS$i/bind/
  sudo mkdir -p $ROOT/ucl_minimal_cfg/NS$i/bind/zones
  sudo mkdir -p /var/log/bind/bind/dns$i.log
- sudo cp $ROOT/dns/utils_dns.py $ROOT/ucl_minimal_cfg/NS$i/bind/
+ #sudo cp $ROOT/dns/utils_dns.py $ROOT/ucl_minimal_cfg/NS$i/bind/
  sudo cp $ROOT/dns/zones/* $ROOT/ucl_minimal_cfg/NS$i/bind/zones
  sudo cp $ROOT/dns/named$i.conf* $ROOT/ucl_minimal_cfg/NS$i/bind/
- sudo cp $ROOT/dns/update_dns.py $ROOT/ucl_minimal_cfg/NS$i/bind/
- chmod +x $ROOT/ucl_minimal_cfg/NS$i/bind/update_dns.py 
+ #sudo cp $ROOT/dns/update_dns.py $ROOT/ucl_minimal_cfg/NS$i/bind/
+ #chmod +x $ROOT/ucl_minimal_cfg/NS$i/bind/update_dns.py 
 done
