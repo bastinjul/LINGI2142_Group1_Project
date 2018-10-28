9c14f93fc8f4616e683d28c226a4042e53ad581e
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-28 15:12:22 +0000

    résolution erreur fichiers reverse dns

--- ucl_minimal_cfg/Carnoy_start.sh
+++ ucl_minimal_cfg/Carnoy_start.sh
@@ -3,15 +3,15 @@
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules 
 
 
-ip link set dev Carnoy-eth2 up 
-ip address add dev Carnoy-eth2 fd00:200:1:fe42::1/64 
-ip address add dev Carnoy-eth2 fd00:300:1:fe42::1/64 
 ip link set dev Carnoy-eth1 up 
 ip address add dev Carnoy-eth1 fd00:200:1:fe41::1/64 
 ip address add dev Carnoy-eth1 fd00:300:1:fe41::1/64 
 ip link set dev Carnoy-eth0 up 
 ip address add dev Carnoy-eth0 fd00:200:1:fe40::1/64 
 ip address add dev Carnoy-eth0 fd00:300:1:fe40::1/64 
+ip link set dev Carnoy-eth2 up 
+ip address add dev Carnoy-eth2 fd00:200:1:fe42::1/64 
+ip address add dev Carnoy-eth2 fd00:300:1:fe42::1/64 
 
 
 ip link set dev Carnoy-lan0 up
