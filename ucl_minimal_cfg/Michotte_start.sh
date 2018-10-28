00d887a6cb6284514d3b23441d20f19b5714769b
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-28 14:23:37 +0000

    modifications DNS

--- ucl_minimal_cfg/Michotte_start.sh
+++ ucl_minimal_cfg/Michotte_start.sh
@@ -3,12 +3,12 @@
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules 
 
 
-ip link set dev Michotte-eth0 up 
-ip address add dev Michotte-eth0 fd00:200:1:ff41::3/64 
-ip address add dev Michotte-eth0 fd00:300:1:ff41::3/64 
 ip link set dev Michotte-eth1 up 
 ip address add dev Michotte-eth1 fd00:200:1:fe40::3/64 
 ip address add dev Michotte-eth1 fd00:300:1:fe40::3/64 
+ip link set dev Michotte-eth0 up 
+ip address add dev Michotte-eth0 fd00:200:1:ff41::3/64 
+ip address add dev Michotte-eth0 fd00:300:1:ff41::3/64 
 
 ip link set dev Michotte-lan0 up 
 ip address add dev Michotte-lan0 fd00:200:1:f740::3/64 
