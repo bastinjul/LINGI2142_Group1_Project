426393108203e9ffe9879297b2278d41c9872b18
Maxmaw <max.mawait@gmail.com>
2018-10-29 13:19:00 +0100

    enhancement test

---
+++ tests/test_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os, subprocess, re, json, sys
+
+PING = 'test_ping6.py'
+WORKING_NODE = 'Nope'
+RUN = './run_test.sh'
+CONFIG = '../ucl_minimal_cfg/'
+
+with open('ip-addr.json') as data_file:
+    data = json.load(data_file)
+
+def test_ping(data):
+    global WORKING_NODE
+    for r in data:
+        WORKING_NODE = r
+        p = subprocess.Popen('{} {} {} {}'.format(RUN, CONFIG, r, PING), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+        print(p.communicate)
+
+test_ping(data)
