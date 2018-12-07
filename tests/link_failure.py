d08a8de214dba5a4e9345748085e99b02b83e6b4
Maxmawt <max.mawait@gmail.com>
2018-12-07 14:38:45 +0100

    improve test

--- tests/link_failure.py
+++ tests/link_failure.py
@@ -2,7 +2,6 @@
 import os, time, json
 from print_res import check
 
-os.chdir('../')
 
 print("!!!!! LINK FAILURE TEST BETWEEN SH1C AND HALLES !!!!!")
 
@@ -10,18 +9,27 @@ os.system('sudo ip netns exec SH1C sudo ip link set SH1C-eth1 down')
 
 time.sleep(30)
 
-os.system('./tests/test_all.py')
+os.system('./test_all.py')
+
+os.chdir('../ucl_minimal_cfg/')
 
 os.system('sudo ip netns exec SH1C sudo ip link set SH1C-eth1 up')
 os.system('sudo ip netns exec SH1C sudo ip address add dev SH1C-eth1 fd00:200:1:ff00::5/64')
 os.system('sudo ip netns exec SH1C sudo ip address add dev SH1C-eth1 fd00:300:1:ff00::5/64')
 
-with open('tests/ping_res.json') as ping_file:
+os.chdir('../tests/')
+
+with open('ping_res.json') as ping_file:
         ping_data = json.load(ping_file)
 
 check(ping_data,'PING')
 
-with open('tests/dig_res.json') as dig_file:
+with open('dig_res.json') as dig_file:
         dig_data = json.load(dig_file)
 
 check(dig_data,'DIG')
+
+with open('bgp_res.json') as bgp_file:
+        bgp_data = json.load(bgp_file)
+
+check(bgp_data,'BGP')
--- tests/standard.py
+++ tests/standard.py
@@ -2,9 +2,13 @@
 import os, json
 from print_res import check
 
+os.chdir('../')
+
 print("!!!!! STANDARD TEST FOR PING AND DIG !!!!!")
 
 os.system('./test_all.py')
+
+os.chdir('tests/')
 
 with open('ping_res.json') as ping_file:
         ping_data = json.load(ping_file)
@@ -15,3 +19,8 @@ with open('dig_res.json') as dig_file:
         dig_data = json.load(dig_file)
 
 check(dig_data,'DIG')
+
+with open('bgp_res.json') as bgp_file:
+        bgp_data = json.load(bgp_file)
+
+check(bgp_data,'BGP')
