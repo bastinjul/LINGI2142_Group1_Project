9f3c487e83576048611c15bfabbaf374c8a76f2d
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-12-07 16:08:36 +0000

    ...

--- dns/deploy_dns.sh
+++ dns/deploy_dns.sh
@@ -9,5 +9,12 @@ do
  sudo mkdir -p $ROOT/ucl_minimal_cfg/NS$i/bind/zones
  sudo mkdir -p /var/log/bind/bind/dns$i.log
  sudo cp $ROOT/dns/zones/* $ROOT/ucl_minimal_cfg/NS$i/bind/zones
- sudo cp $ROOT/dns/named.conf* $ROOT/ucl_minimal_cfg/NS$i/bind/
+ sudo cp $ROOT/dns/named$i.conf* $ROOT/ucl_minimal_cfg/NS$i/bind/
+done
+
+for i in 1 2
+do
+ sudo mv $ROOT/ucl_minimal_cfg/NS$i/bind/named$i.conf $ROOT/ucl_minimal_cfg/NS$i/bind/named.conf
+ sudo mv $ROOT/ucl_minimal_cfg/NS$i/bind/named$i.conf.local $ROOT/ucl_minimal_cfg/NS$i/bind/named.conf.local
+ sudo mv $ROOT/ucl_minimal_cfg/NS$i/bind/named$i.conf.options $ROOT/ucl_minimal_cfg/NS$i/bind/named.conf.options
 done
--- ucl_minimal_cfg/Pythagore_start.sh
+++ ucl_minimal_cfg/Pythagore_start.sh
@@ -19,12 +19,12 @@ ip link set dev Pythagore-eth0 up
 ip address add dev Pythagore-eth0 fd00:200:1:ff02::4/64 
 ip address add dev Pythagore-eth0 fd00:300:1:ff02::4/64 
 
-ip link set dev Pythagore-lan0 up 
-ip address add dev Pythagore-lan0 fd00:200:1:f600::4/64 
-ip address add dev Pythagore-lan0 fd00:300:1:f600::4/64 
 ip link set dev Pythagore-lan1 up 
 ip address add dev Pythagore-lan1 fd00:200:1:f61f::4/64 
 ip address add dev Pythagore-lan1 fd00:300:1:f61f::4/64 
+ip link set dev Pythagore-lan0 up 
+ip address add dev Pythagore-lan0 fd00:200:1:f600::4/64 
+ip address add dev Pythagore-lan0 fd00:300:1:f600::4/64 
 
 ip link set dev Pythagore-lan2 up
 ip link add link Pythagore-lan2 name Pyth-lan2.000 type vlan id 0x000
